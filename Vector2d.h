4f54a4be8773956d645b7b245d25128850f5be38
mrtycjan <mrtycjan@protonmail.ch>
2016-04-15 09:41:34 +0200

    Make x, y and constructor public

--- Vector2d.h
+++ Vector2d.h
@@ -4,14 +4,13 @@
 
 class Vector2d
 {
-private:
+public:
 
 	float x;
 	float y;
 
-	Vector2d(float X, float Y);
+	Vector2d(float=0, float=0);
 
-public:
 
 	static float radiansToDegrees(float radians); //Convert Radians into Degrees
 
@@ -25,10 +24,10 @@ public:
 
 	static float rotationInDegreesFromVector(float fromX, float fromY, float toX, float toY); //Compute vector and rotation from vector, returns rotation in degrees
 
-	static Vector2d pointToPointVector(float toX, float toY, float fromX, float fromY, float deltaTime = 1, float velocity = 1); //Compute vector, optional arguments, deltaTime, velocity
+	static Vector2d pointToPointVector(float toX, float toY, float fromX, float fromY, float deltaTime = 1, float velocity = 1); //Compute vector, optional arguments, deltaTime, object velocity
 	
-	static Vector2d vectorFromRotationInRadians(float rotationInRadians, float deltaTime = 1, float velocity = 1); //Compute vector from rotation in radians, optional arguments, deltaTime, velocity
+	static Vector2d vectorFromRotationInRadians(float rotationInRadians, float deltaTime = 1, float velocity = 1); //Compute vector from rotation in radians, optional arguments, deltaTime, object velocity
 
-	static Vector2d vectorFromRotationInDegrees(float rotationInDegrees, float deltaTime = 1, float velocity = 1); //Compute vector from rotation in degrees, optional arguments, deltaTime, velocity
+	static Vector2d vectorFromRotationInDegrees(float rotationInDegrees, float deltaTime = 1, float velocity = 1); //Compute vector from rotation in degrees, optional arguments, deltaTime, object velocity
 
 };
